715cc02242fab69b560ef201e573dbb4aecdf723
vaibhavi66 <144114821+vaibhavi66@users.noreply.github.com>
2026-03-16 14:56:23 +0530

    selflearning question 1 to 4

---
+++ SELFQ_4.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class Laptop
+{
+private:
+    int brandId;
+    float price;
+
+public:
+    
+    Laptop()
+    {
+        brandId = 1;
+        price = 50000;
+        cout << "Laptop Constructor Called" << endl;
+    }
+
+    
+    void display()
+    {
+        cout << "Brand ID : " << brandId << endl;
+        cout << "Price : " << price << endl;
+    }
+
+    
+    ~Laptop()
+    {
+        cout << "Laptop Destructor Called" << endl;
+    }
+};
+
+int main()
+{
+    Laptop l1;   
+
+    l1.display();
+
+    return 0;
+}
